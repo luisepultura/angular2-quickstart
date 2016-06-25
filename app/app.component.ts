760d4ed2b249f08f0595adb9559a6c288eb3dabf
luis u <luisepultura@hotmail.com>
2016-06-25 08:24:47 +0100

    created app compnent html and extended app component ts file

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,22 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html'
+    
 })
-export class AppComponent { }
+export class AppComponent {
+    //[] means property binding; one way up from Component to DOM
+    //() means event binding; DOM to Component
+
+    //{{title}}, one way data binding from component to template
+    title = 'Customer App';
+    name = 'Momo';
+    /*  binding to style property of the div, in this case color property of style
+        bind to component to any property in DOM
+    */
+    momoColor = 'blue';
+
+    changeColor() {
+        this.momoColor = this.momoColor === 'blue' ? 'red' : 'blue';
+    }
+}
